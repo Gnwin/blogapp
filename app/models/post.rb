3f653765e0cbb31bfb54e1d784742b39f861afa0
Godwin Nwachukwu <nwgwin@gmail.com>
2022-07-23 14:34:37 +0100

    create custom methods for models

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   belongs_to :user
+
+  def increment_user_posts_count
+    user.increment!(:posts_counter)
+  end
+
+  def recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
 end
