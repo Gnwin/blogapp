0c2bf189f2aeefdc0bf3add8ea83148c5c9c647c
Godwin Nwachukwu <nwgwin@gmail.com>
2022-08-12 06:41:00 +0100

    setup auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # def authenticate_user!
+  #   if user_signed_in?
+  #     super
+
+  #   else
+  #     redirect_to new_user_session_path, notice: 'Please sign in'
+  # end
 end
