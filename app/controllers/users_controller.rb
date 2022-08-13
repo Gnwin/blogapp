27498638a9f9a9fb489c6c4f3d96d11ee2804fe0
Godwin Nwachukwu <nwgwin@gmail.com>
2022-08-14 00:47:49 +0100

    change user to current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,13 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all
+    # @users = User.all
+    @users = current_user
   end
 
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    @user = current_user
     @recent_posts = @user.recent_posts
   end
 end
